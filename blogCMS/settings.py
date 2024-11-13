38b466e3a0f49a3383030c69791e216382406536
Selin Çelebi <selincelebi@Selin-MacBook-Pro.local>
2024-11-13 18:42:05 +0300

    Update

--- blogCMS/settings.py
+++ blogCMS/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','selincelebi.xyz', 'www.selincelebi.xyz']
+ALLOWED_HOSTS = []
 
 
 
