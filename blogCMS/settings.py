a538d7530c0409bd0e28994ec13c7ee10515ebf2
Selin Çelebi <selincelebi@Selin-MacBook-Pro.local>
2024-11-14 12:22:32 +0300

    update

--- blogCMS/settings.py
+++ blogCMS/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','selincelebi.xyz', 'www.selincelebi.xyz']
+ALLOWED_HOSTS = []
 
 
 
